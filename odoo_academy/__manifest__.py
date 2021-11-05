743075ea7d960d2a186f62dc4c98cfbb3173b757
kelsinglobal <admin@kelsinglobal.com>
2021-11-05 22:45:14 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,20 @@
+# -*-  coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    'version': '0.1',
+    'description':""" 
+        Academy Module to manage Training:
+        \* Courses
+        \* Sessions
+        \* Attendees
+    """,
+    'author': 'Odoo',
+    'website': 'https://Odoo.com',
+    'category': 'Trining',
+    'depends': ['base'],
+    'data':[        
+    ],
+    'demo':[        
+    ],
+}
